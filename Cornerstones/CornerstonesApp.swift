b3c7d07690383d66fc33a4bb0fdf754dcfdf6a47
Jay Son <json@lookscopeapp.com>
2023-12-24 14:23:10 -0500

    initial commit

---
+++ Cornerstones/CornerstonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornerstonesApp.swift
+//  Cornerstones
+//
+//  Created by Jay Son on 2023-12-24.
+//
+
+import SwiftUI
+
+@main
+struct CornerstonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
